f81b06720478c486dc031e2d1f695830b00c0928
Lin He <he9lin@gmail.com>
2013-02-28 13:36:11 -0800

    Move raise exception to FormShui module.

--- lib/faraday/raise_http_exception.rb
+++ lib/faraday/raise_http_exception.rb
@@ -1,51 +1,53 @@
 require 'faraday'
 
-module FaradayMiddleware
-  class RaiseHttpException < Faraday::Middleware
-    def call(env)
-      @app.call(env).on_complete do |response|
-        case response[:status].to_i
-        when 400
-          raise FormShui::BadRequest, error_message_400(response)
-        when 401
-          raise FormShui::UnauthorizedAccess, error_message_400(response)
-        when 403
-          raise FormShui::ForbiddenAccess, error_message_400(response)
-        when 404
-          raise FormShui::NotFound, error_message_400(response)
-        when 500
-          raise FormShui::InternalServerError, error_message_500(response, "Something is technically wrong.")
-        when 503
-          raise FormShui::ServiceUnavailable, error_message_500(response, "FormShui is rate limiting your requests.")
+module FormShui
+  module FaradayMiddleware
+    class RaiseHttpException < Faraday::Middleware
+      def call(env)
+        @app.call(env).on_complete do |response|
+          case response[:status].to_i
+          when 400
+            raise FormShui::BadRequest, error_message_400(response)
+          when 401
+            raise FormShui::UnauthorizedAccess, error_message_400(response)
+          when 403
+            raise FormShui::ForbiddenAccess, error_message_400(response)
+          when 404
+            raise FormShui::NotFound, error_message_400(response)
+          when 500
+            raise FormShui::InternalServerError, error_message_500(response, "Something is technically wrong.")
+          when 503
+            raise FormShui::ServiceUnavailable, error_message_500(response, "FormShui is rate limiting your requests.")
+          end
         end
       end
-    end
 
-    def initialize(app)
-      super app
-      @parser = nil
-    end
-
-    private
-
-    def error_message_400(response)
-      "#{response[:method].to_s.upcase} #{response[:url].to_s}: #{response[:status]}#{error_body(response[:body])}"
-    end
-
-    def error_body(body)
-      if not body.nil? and not body.empty? and body.kind_of?(String)
-        body = ::JSON.parse(body)
+      def initialize(app)
+        super app
+        @parser = nil
       end
 
-      if body.nil?
-        nil
-      elsif body['meta'] and body['meta']['error_message'] and not body['meta']['error_message'].empty?
-        ": #{body['meta']['error_message']}"
-      end
-    end
+      private
 
-    def error_message_500(response, body=nil)
-      "#{response[:method].to_s.upcase} #{response[:url].to_s}: #{[response[:status].to_s + ':', body].compact.join(' ')}"
+      def error_message_400(response)
+        "#{response[:method].to_s.upcase} #{response[:url].to_s}: #{response[:status]}#{error_body(response[:body])}"
+      end
+
+      def error_body(body)
+        if not body.nil? and not body.empty? and body.kind_of?(String)
+          body = ::JSON.parse(body)
+        end
+
+        if body.nil?
+          nil
+        elsif body['meta'] and body['meta']['error_message'] and not body['meta']['error_message'].empty?
+          ": #{body['meta']['error_message']}"
+        end
+      end
+
+      def error_message_500(response, body=nil)
+        "#{response[:method].to_s.upcase} #{response[:url].to_s}: #{[response[:status].to_s + ':', body].compact.join(' ')}"
+      end
     end
   end
 end
