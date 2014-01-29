bdfb84df0cb76a17aed9913dbe148828a495e6d7
Bryan B. Cabalo <bcabalo@gmail.com>
2014-01-29 12:22:44 -0800

    Support answers total count fetch on form id

--- lib/form_shui/models/answer.rb
+++ lib/form_shui/models/answer.rb
@@ -2,5 +2,10 @@ module FormShui
   class Answer < Base
     path_name  "/forms/:form_id/answers"
     root_param "answer"
+
+    def self.find_answers_count_by_form_id(id)
+      response = request.get("/forms/#{id}/answers/count", nil)
+      response.body
+    end
   end
 end
