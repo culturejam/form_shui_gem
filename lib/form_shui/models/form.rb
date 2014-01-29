fb7685103b2b5ba118f9c5d18a8a5b5b7ca3e1ae
Bryan B. Cabalo <bcabalo@gmail.com>
2014-01-29 11:43:53 -0800

    Support Management to consume new Formshui submissions filed registry endpoint

--- lib/form_shui/models/form.rb
+++ lib/form_shui/models/form.rb
@@ -3,6 +3,11 @@ module FormShui
     path_name  "/forms"
     root_param "form"
 
+    def self.find_form_columns_by_form_id(id)
+      response = request.get("/forms/#{id}/field_registry", nil)
+      JSON.parse(response.body)
+    end
+
     def self.find_by_organization_id(id)
       response = request.get "/organizations/#{id}/forms"
       parse_doc(response.body)
