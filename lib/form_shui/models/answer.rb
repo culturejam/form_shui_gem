5deef86808bab501549af18713f95efba25aa7d4
Bryan B. Cabalo <bcabalo@gmail.com>
2014-07-02 17:42:37 -0700

    Support passing sort params

--- lib/form_shui/models/answer.rb
+++ lib/form_shui/models/answer.rb
@@ -17,10 +17,13 @@ module FormShui
       form_id = args[0]
       page = args[1]
       per_page = args[2]
+      sort_by = args[3]
+      order = args[4]
       options_hash = args.last.is_a?(Hash) ? args.pop : {}
 
       response = request.get(
-        "/forms/#{form_id}/answers?page=#{page}&per_page=#{per_page}", nil
+        "/forms/#{form_id}/answers?page=#{page}&per_page=#{per_page}" \
+        "&sort_by=#{sort_by}&sort_order=#{order}", nil
       )
 
       response.body
